1522361a1b0a4b92eb7d3e5bd15152710d5ef3b1
Daniel Young <danyoung@utexas.edu>
2024-07-23 09:42:34 -0700

    Added github actions for automatic linting and unit test running. Added dummy unit test

--- src/prsdk/dummy.py
+++ src/prsdk/dummy.py
@@ -1,2 +1,8 @@
+"""
+A dummy module to test the package.
+"""
 def compute_percent_change(x: int) -> float:
+    """
+    A function that divides an integer by 100 and returns a float.
+    """
     return x / 100
